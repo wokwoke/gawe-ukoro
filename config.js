3f84625f9dbb3b310cf0b33e7a1d6c95d0fe2693
wokwoke <cosm45rudy@gmail.com>
2026-06-19 08:30:12 +0700

    feat: supabase integration + referensi lagu

---
+++ config.js
@@ -0,0 +1,5 @@
+cd ~/gawe-ukoro
+cat > config.js << 'EOF'
+const SUPABASE_URL = 'https://acdtjtvqitbcvqlqttcd.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_n4uOhif1lGGCBoDaK4Njcw_iRVHu08r';
+EOF
